f83e7af85e3e29eefc4d5230e1d07dac86b6c4c8
Michele Bersani <49282359+michelebersani@users.noreply.github.com>
2021-06-29 18:48:25 +0200

    Failed gpu OSX

--- lstm.py
+++ lstm.py
@@ -1,13 +1,7 @@
 import numpy as np
 import pickle
-import keras
-
-###### MAC PARALLELIZATION #######
-import os
-from keras import backend as K
-K
-os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
-##################################
+import tensorflow as tf
+from tensorflow import keras
 
 modelName = "2_layers_biLSTM"
 
@@ -21,21 +15,22 @@ features = 300
 xTrain = np.reshape(xTrain,(-1, maxTrainingLength, features))
 
 xVal = np.load('data/x_val.npy',allow_pickle=True)
-yVal = np.load('data/y_val.npy',allow_pickle=True)
 xVal = keras.preprocessing.sequence.pad_sequences(
-    xVal, maxlen=maxTrainingLength, padding="post", dtype=float)
-xVal = np.reshape(xVal,(-1, maxTrainingLength, features))
+    xVal, padding="post", dtype=float)
+maxValLength = len(xVal[0])
+features = 300
+xVal = np.reshape(xVal,(-1, maxValLength, features))
+yVal = np.load('data/y_val.npy',allow_pickle=True)
 
 model = keras.Sequential([
-    keras.layers.Masking(mask_value=0.,input_shape=(maxTrainingLength, features)),
-    keras.layers.Bidirectional(keras.layers.LSTM(128, dropout=0.2, recurrent_dropout=0.4, return_sequences=True), input_shape=(None, 300), name='bidirectional_1'),
-    keras.layers.Masking(mask_value=0.,input_shape=(maxTrainingLength, 128)),
-    keras.layers.Bidirectional(keras.layers.LSTM(128, dropout=0.2, recurrent_dropout=0.4), name='bidirectional_2'),
+    keras.layers.Masking(mask_value=0.,input_shape=(None, features)),
+    keras.layers.Bidirectional(keras.layers.LSTM(128, dropout=0.2, return_sequences=True), input_shape=(None, 300), name='bidirectional_1'),
+    keras.layers.Bidirectional(keras.layers.LSTM(128, dropout=0.2), name='bidirectional_2'),
     keras.layers.Dense(300, activation='relu', name='dense_1'),
     keras.layers.Dense(100, activation='relu', name='dense_2'),
-    keras.layers.Dense(24, activation='sigmoid', name='output')
+    keras.layers.Dense(24, name='output')
 ])
-model.compile(loss=keras.losses.BinaryCrossentropy(),
+model.compile(loss=keras.losses.BinaryCrossentropy(from_logits=True),
               optimizer=keras.optimizers.Adam(1e-4),
               metrics=['accuracy'])
 
